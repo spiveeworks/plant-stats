5f3c3eda5c242b2319de73eb79cfa23965e50229
SpiveeWorks <spiveehere@gmail.com>
2019-03-28 12:52:21 +1100

    circle

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,33 @@
 use piston_window::*;
 
 struct Game {
+    player_pos: [f64; 2],
+    // is there a piston feature for this?
+    view_pos: [f64; 2],
 }
 
 
-impl piston_app::App for Game {
+impl piston_app::Draw for Game {
     fn on_draw(
         self: &mut Self,
-        _centre: Context,
-        _graphics: &mut G2d,
-        _args: RenderArgs,
+        mut centre: math::Matrix2d,
+        _dt: f64,
+        graphics: &mut G2d,
     ) {
+        clear([0.0, 0.0, 0.0, 1.0], graphics);
+
+        // think of this as finding the in-game location of the screen centre
+        centre = centre.trans(-self.view_pos[0], -self.view_pos[1]);
+
+        let player = centre.trans(self.player_pos[0], self.player_pos[1]);
+
+        let color = [1.0, 0.0, 0.0, 1.0];
+        let rect = [-10.0, -10.0, 20.0, 20.0];
+        ellipse(color, rect, player, graphics);
     }
+}
+
+impl piston_app::App for Game {
     fn on_update(
         self: &mut Self,
         _args: UpdateArgs,
@@ -39,6 +55,9 @@ impl piston_app::App for Game {
 
 
 fn main() {
-    let game = Game {};
+    let game = Game {
+        player_pos: [0.0, 0.0],
+        view_pos: [0.0, 0.0],
+    };
     piston_app::run_until_escape(game);
 }
